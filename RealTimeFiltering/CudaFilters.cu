8831e509ba27df71c3a59f616a3ed1bb0ce45c8e
unknown <jawien.janek@gmail.com>
2026-05-28 15:27:58 +0200

    Added posterize, added median, fixed wrong arguments in kernel calls in runCuda

--- RealTimeFiltering/CudaFilters.cu
+++ RealTimeFiltering/CudaFilters.cu
@@ -334,6 +334,127 @@ __global__ void sobelKernel(unsigned char* data, int width, int height) {
     data[outOffset + 2] = edge;
 }
 
+__global__ void median3x3(unsigned char* data, int sizeV, int sizeH, float mix) {
+    if(mix == 0.0f) return;
+
+    const int sharedSize = TILE_WIDTH + 2;
+    __shared__ unsigned char
+        tile[sharedSize][sharedSize][3];
+
+    // Flattened thread id
+    int tid = threadIdx.y * TILE_WIDTH + threadIdx.x;
+
+    // Global pixel coordinates
+    int row = blockIdx.y * TILE_WIDTH + threadIdx.y;
+    int col = blockIdx.x * TILE_WIDTH + threadIdx.x;
+
+    // Load values into shared memory
+    for (int index = tid; 
+            index < sharedSize * sharedSize; 
+            index += TILE_WIDTH * TILE_WIDTH) {
+        int sRow = index / sharedSize;
+        int sCol = index % sharedSize;
+        int gRow = blockIdx.y * TILE_WIDTH + sRow - 1;
+        int gCol = blockIdx.x * TILE_WIDTH + sCol - 1;
+
+        if (gRow >= 0 && gRow < sizeV && gCol >= 0 && gCol < sizeH) {
+            int globalOffset = (gRow * sizeH + gCol) * 3;
+            tile[sRow][sCol][0] = data[globalOffset + 0]; // B
+            tile[sRow][sCol][1] = data[globalOffset + 1]; // G
+            tile[sRow][sCol][2] = data[globalOffset + 2]; // R
+        } else {
+            tile[sRow][sCol][0] = 0;
+            tile[sRow][sCol][1] = 0;
+            tile[sRow][sCol][2] = 0;
+        }
+    }
+
+    __syncthreads();
+
+    // Apply 3x3 filter
+    if (row < sizeV && col < sizeH)
+    {
+        unsigned char b[9];
+        unsigned char g[9];
+        unsigned char r[9];
+
+        int k = 0;
+        #pragma unroll
+        for (int dy = 0; dy < 3; ++dy) {
+            #pragma unroll
+            for (int dx = 0; dx < 3; ++dx) {
+                int sy = ty + dy;
+                int sx = tx + dx;
+                unsigned char vb = tile[sy][sx][0];
+                unsigned char vg = tile[sy][sx][1];
+                unsigned char vr = tile[sy][sx][2];
+
+                int i = k - 1;
+                while (i >= 0 && b[i] > vb) {
+                    b[i + 1] = b[i];
+                    i--;
+                }
+                b[i + 1] = vb;
+
+                i = k - 1;
+                while (i >= 0 && g[i] > vg) {
+                    g[i + 1] = g[i];
+                    i--;
+                }
+                g[i + 1] = vg;
+
+                i = k - 1;
+                while (i >= 0 && r[i] > vr) {
+                    r[i + 1] = r[i];
+                    i--;
+                }
+                r[i + 1] = vr;
+
+                k++;
+            }
+        }
+
+        if (row == 0 || col == 0 || row == height - 1 || col == width - 1)
+            return;
+
+        int outputOffset = (row * sizeH + col) * 3;
+        data[outputOffset + 0] = b[4]*mix + tile[ty + 1][tx + 1][0]*(1.0f-mix);
+        data[outputOffset + 1] = g[4]*mix + tile[ty + 1][tx + 1][1]*(1.0f-mix);
+        data[outputOffset + 2] = r[4]*mix + tile[ty + 1][tx + 1][2]*(1.0f-mix);
+    }
+}
+
+__global__ void posterize(unsigned char* data, int height, int width, float degree) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x >= width || y >= height)
+        return;
+
+    int pixelIdx = y * width + x;
+    int offset = pixelIdx * 3;
+
+    // Exponential perceptual mapping:
+    // 0.0 -> 256 levels
+    // 0.5 -> 16 levels
+    // 1.0 -> 2 levels
+    float exponent = 8.0f * (1.0f - degree);
+    int levels = max(2, (int)roundf(powf(2.0f, exponent)));
+    float step = 255.0f / (levels - 1);
+
+    // loops over 3 channels
+    #pragma unroll
+    for (int c = 0; c < 3; ++c) {
+        float value = (float)data[offset + c];
+        value = roundf(value / step) * step;
+        data[offset + c] = (unsigned char)(
+            fminf(255.0f, fmaxf(0.0f, value))
+        );
+    }
+}
+
+
+
 // 2. ZMIENNE I FUNKCJE POMOCNICZE
 unsigned char* d_buffer = nullptr;
 int currentBufferSize = 0;
@@ -499,7 +620,7 @@ void runCuda(unsigned char* data, int width, int height, float mixFactors[10]) {
             {1.f/9, 1.f/9, 1.f/9},
             {1.f/9, 1.f/9, 1.f/9}
         }, sizeof(float) * 9);
-        filter3x3<<<dimGrid, dimBlock>>> (d_buffer, height, width, mix);
+        filter3x3<<<dimGrid, dimBlock>>> (d_buffer, height, width, mixFactors[1]);
     }
     if(mixFactors[2] >= 0.0f) {
         cudaMemcpyToSymbol(c_filter, {
@@ -507,10 +628,14 @@ void runCuda(unsigned char* data, int width, int height, float mixFactors[10]) {
             {-1.f,  5.f, -1.f },
             { 0.f, -1.f,  0.f }
         }, sizeof(float) * 9);
-        filter3x3<<<dimGrid, dimBlock>>> (d_buffer, height, width, mix);
+        filter3x3<<<dimGrid, dimBlock>>> (d_buffer, height, width, mixFactors[2]);
     }
     if(mixFactors[3] >= 0.0f)
-        SobelKernel<<<blocksPerGrid, threadsPerBlock>>>(d_buffer, numPixels, (unsigned char)(255*mixFactors[0]));
+        SobelKernel<<<blocksPerGrid, threadsPerBlock>>>(d_buffer, width, height);
+    if(mixFactors[4] >= 0.0f)
+        median3x3<<<blocksPerGrid, threadsPerBlock>>>(d_buffer, height, width, mixFactors[4]);
+    if(mixFactors[5] >= 0.0f)
+        posterize<<<blocksPerGrid, threadsPerBlock>>>(d_buffer, height, width, mixFactors[5]);
     
 
     
